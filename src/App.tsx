5ebf844f6bb0d551ab101bb1e43835ae2f51548f
Alexander Edemskii <a_edemskii@EdemMBP.local>
2024-04-03 20:31:29 +0300

    useAppContext hook for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { AppContextProvider } from './assets/hooks/useAppContext'
 import './App.css'
 
 const App: React.FC = () => {
 
   return (
-    <>
-    </>
+    <AppContextProvider>
+      <h1>Child</h1>
+    </AppContextProvider>
   )
 }
 
---
+++ src/assets/hooks/useAppContext.tsx
@@ -0,0 +1,59 @@
+import React, { useReducer, useContext } from 'react';
+
+const AppContext = React.createContext<AppStateContext|null>(null);
+
+export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const initialAppState: AppState = 'INTRO'
+  const [ state, dispatch ] = useReducer(appStateReducer, initialAppState);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (context === null) {
+    throw new Error('useChartContext must be used within a ChartContextProvider');
+  }
+  return context;
+};
+
+const appStateReducer = (appState: AppState, action: AppReducerAction): AppState => {
+  switch (action.type) {
+    case APP_REDUCER_ACTION.SPIN_WINDMILL:
+      return APP_STATE.WINDMILL_SPINNING;
+    case APP_REDUCER_ACTION.AWAIT_USER:
+      return APP_STATE.AWAITING_USER;
+    default:
+      return appState;
+  }
+};
+
+export type AppStateContext = {
+  state: AppState;
+  dispatch: React.Dispatch<AppReducerAction>;
+};
+
+type ObjectValues<T> = T[keyof T];
+
+export type AppState = ObjectValues<typeof APP_STATE>;
+
+export type AppReducerAction = {
+  type: ObjectValues<typeof APP_REDUCER_ACTION>;
+};
+
+export const APP_STATE = {
+  NONE: 'NONE',
+  INTRO: 'INTRO',
+  AWAITING_USER: 'AWAITING_USER',
+  WINDMILL_SPINNING: 'WINDMILL_SPINNING',
+} as const;
+
+export const APP_REDUCER_ACTION = {
+  SPIN_WINDMILL: 'SPIN_WINDMILL',
+  AWAIT_USER: 'AWAIT_USER',
+  PLAY_INTRO: 'PLAY_INTRO',
+} as const;
